26d44185583ba0879156966b13a92a0b60a1c179
Kesara Wimal <kesarawimal9@gmail.com>
2018-11-16 22:54:01 +0530

    tooltips added

--- src/panels/index.js
+++ src/panels/index.js
@@ -40,14 +40,29 @@ export default (editor, config) => {
       context: edt,
       command:  e => e.runCommand(cmdEdit),
       className: 'fa fa-code',
+        attributes: {
+        'data-tooltip': 'Edit Code',
+            'title': 'Edit Code',
+            'data-tooltip-pos': 'bottom',
+    },
     },{
       id: 'undo',
       className: 'fa fa-undo',
       command: e => e.runCommand('core:undo'),
+        attributes: {
+            'data-tooltip': 'Undo',
+            'title': 'Undo',
+            'data-tooltip-pos': 'bottom',
+        },
     },{
       id: 'redo',
       className: 'fa fa-repeat',
       command: e => e.runCommand('core:redo'),
+        attributes: {
+            'data-tooltip': 'Redo',
+            'title': 'Redo',
+            'data-tooltip-pos': 'bottom',
+        },
     }],
   },{
     id: 'views',
@@ -56,10 +71,20 @@ export default (editor, config) => {
       command: osm,
       active: true,
       className: 'fa fa-paint-brush',
+        attributes: {
+            'data-tooltip': 'Options',
+            'title': 'Options',
+            'data-tooltip-pos': 'bottom',
+        },
     },{
       id: obl,
       command: obl,
       className: 'fa fa-th-large',
+        attributes: {
+            'data-tooltip': 'Elements',
+            'title': 'Elements',
+            'data-tooltip-pos': 'bottom',
+        },
     }],
   }]);
 
@@ -70,24 +95,49 @@ export default (editor, config) => {
       context: prv,
       command: e => e.runCommand(prv),
       className: 'fa fa-eye',
+      attributes: {
+          'data-tooltip': 'Preview',
+          'title': 'Preview',
+          'data-tooltip-pos': 'bottom',
+      },
   },{
       id: ful,
       command: ful,
       context: ful,
       className: 'fa fa-arrows-alt',
+      attributes: {
+          'data-tooltip': 'Full Screen',
+          'title': 'Full Screen',
+          'data-tooltip-pos': 'bottom',
+      },
   },{
     id: cmdDeviceDesktop,
     command: cmdDeviceDesktop,
     className: 'fa fa-desktop',
     active: 1,
+      attributes: {
+          'data-tooltip': 'Desktop',
+          'title': 'Desktop',
+          'data-tooltip-pos': 'bottom',
+      },
   },{
     id: cmdDeviceTablet,
     command: cmdDeviceTablet,
     className: 'fa fa-tablet',
+      attributes: {
+          'data-tooltip': 'Tablet',
+          'title': 'Tablet',
+          'data-tooltip-pos': 'bottom',
+      },
   },{
     id: cmdDeviceMobile,
     command: cmdDeviceMobile,
     className: 'fa fa-mobile',
+      attributes: {
+          'data-tooltip': 'Mobile',
+          'title': 'Mobile',
+          'data-tooltip-pos': 'bottom',
+      },
   }]);
 
   const openBl = pn.getButton('views', obl);
