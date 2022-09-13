0b841e10608278de9fceba3f1bf2587dd4ced924
Artur Arseniev <artur.catch@hotmail.it>
2022-09-13 16:34:53 +0400

    Update panel device icons

--- src/panels.ts
+++ src/panels.ts
@@ -30,6 +30,29 @@ export default (editor: grapesjs.Editor, config: RequiredPluginOptions) => {
       buttons: [{}],
     },
     {
+      id: 'devices-c',
+      buttons: [{
+        id: cmdDeviceDesktop,
+        command: cmdDeviceDesktop,
+        active: true,
+        label: `<svg style="display: block" viewBox="0 0 24 24">
+            <path fill="currentColor" d="M21,16H3V4H21M21,2H3C1.89,2 1,2.89 1,4V16A2,2 0 0,0 3,18H10V20H8V22H16V20H14V18H21A2,2 0 0,0 23,16V4C23,2.89 22.1,2 21,2Z" />
+        </svg>`
+      },{
+        id: cmdDeviceTablet,
+        command: cmdDeviceTablet,
+        label: `<svg style="display: block" viewBox="0 0 24 24">
+            <path fill="currentColor" d="M19,18H5V6H19M21,4H3C1.89,4 1,4.89 1,6V18A2,2 0 0,0 3,20H21A2,2 0 0,0 23,18V6C23,4.89 22.1,4 21,4Z" />
+        </svg>`
+      },{
+        id: cmdDeviceMobile,
+        command: cmdDeviceMobile,
+        label: `<svg style="display: block" viewBox="0 0 24 24">
+            <path fill="currentColor" d="M17,19H7V5H17M17,1H7C5.89,1 5,1.89 5,3V21A2,2 0 0,0 7,23H17A2,2 0 0,0 19,21V3C19,1.89 18.1,1 17,1Z" />
+        </svg>`
+      }]
+    },
+    {
       id: 'options',
       buttons: [{
         id: swv,
@@ -109,25 +132,6 @@ export default (editor: grapesjs.Editor, config: RequiredPluginOptions) => {
           <path fill="currentColor" d="M17,13H13V17H11V13H7V11H11V7H13V11H17M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3Z" />
       </svg>`
     }],
-  }]);
-
-  // Add devices buttons
-  const panelDevices = Panels.addPanel({id: 'devices-c'});
-
-  // @ts-ignore
-  panelDevices.get('buttons').add([{
-    id: cmdDeviceDesktop,
-    command: cmdDeviceDesktop,
-    className: 'fa fa-desktop',
-    active: true,
-  },{
-    id: cmdDeviceTablet,
-    command: cmdDeviceTablet,
-    className: 'fa fa-tablet',
-  },{
-    id: cmdDeviceMobile,
-    command: cmdDeviceMobile,
-    className: 'fa fa-mobile',
   }]);
 
   const openBl = Panels.getButton('views', obl);
