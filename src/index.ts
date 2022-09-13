8e4a35f849e9f5e469146c5c2e66590205b18e4d
Artur Arseniev <artur.catch@hotmail.it>
2022-09-13 16:52:01 +0400

    Remove style manager stuff

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,11 @@ import type grapesjs from 'grapesjs';
 import commands from './commands';
 import blocks from './blocks';
 import panels from './panels';
-import styles from './styles';
 
 export type PluginOptions = {
   /**
    * Which blocks to add.
-   * @default ['column1', 'column2', 'column3', 'column3-7', 'text', 'link', 'image', 'video', 'map']
+   * @default ['link-block', 'quote', 'text-basic']
    */
   blocks?: string[];
 
@@ -60,7 +59,6 @@ export type RequiredPluginOptions = Required<PluginOptions>;
 
 const plugin: grapesjs.Plugin<PluginOptions> = (editor, opts: Partial<PluginOptions> = {}) => {
   const config: RequiredPluginOptions = {
-    // Which blocks to add
     blocks: ['link-block', 'quote', 'text-basic'],
     modalImportTitle: 'Import',
     modalImportButton: 'Import',
@@ -69,24 +67,6 @@ const plugin: grapesjs.Plugin<PluginOptions> = (editor, opts: Partial<PluginOpti
     importViewerOptions: {},
     textCleanCanvas: 'Are you sure you want to clear the canvas?',
     showStylesOnChange: true,
-
-    // // Text for General sector in Style Manager
-    // textGeneral: 'General',
-
-    // // Text for Layout sector in Style Manager
-    // textLayout: 'Layout',
-
-    // // Text for Typography sector in Style Manager
-    // textTypography: 'Typography',
-
-    // // Text for Decorations sector in Style Manager
-    // textDecorations: 'Decorations',
-
-    // // Text for Extra sector in Style Manager
-    // textExtra: 'Extra',
-
-    // Use custom set of sectors for the Style Manager
-    // customStyleManager: [],
     ...opts,
   };
 
@@ -98,10 +78,6 @@ const plugin: grapesjs.Plugin<PluginOptions> = (editor, opts: Partial<PluginOpti
 
   // Load panels
   panels(editor, config);
-
-  // Load styles
-  styles(editor, config);
-
 }
 
 export default plugin;
