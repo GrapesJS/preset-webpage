6fd5ba2ab0fa51ba94c8807ac0b11082e12e59a8
Artur Arseniev <artur.catch@hotmail.it>
2022-09-13 14:14:18 +0400

    Up consts file

--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,5 @@
-export const
-  cmdImport = 'gjs-open-import-webpage',
-  cmdDeviceDesktop = 'set-device-desktop',
-  cmdDeviceTablet = 'set-device-tablet',
-  cmdDeviceMobile = 'set-device-mobile',
-  cmdClear = 'canvas-clear';
+export const cmdImport = 'gjs-open-import-webpage';
+export const cmdDeviceDesktop = 'set-device-desktop';
+export const cmdDeviceTablet = 'set-device-tablet';
+export const cmdDeviceMobile = 'set-device-mobile';
+export const cmdClear = 'canvas-clear';
